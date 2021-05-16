29d31efd467df741cf34a059e06bb6bfdbf20b18
Alejo <vera.alejo0903@gmail.com>
2021-05-15 21:18:41 -0300

    Rendering array

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
     return (
         <div>
-            <h1>Hola Header</h1>
+            <h1>Hola</h1>
         </div>
     )
 }
--- src/Table.js
+++ src/Table.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import TableContent from './TableContent';
+
 
 
 const useStyles = makeStyles(  ({
@@ -19,30 +20,26 @@ const useStyles = makeStyles(  ({
   }));
 
 function Table() {
+
+    
+    const [count, setCount] = useState([0,1,2])
+    
+    
+
+
     const classes = useStyles();
     return (
             <div className={classes.root}>
                 <Grid container spacing={3}>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <TableContent />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <TableContent />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <TableContent />
-                        </Paper>
-                    </Grid>
-                    <Grid item xs={6}>
-                        <Paper className={classes.paper}>
-                            <TableContent />
-                        </Paper>
-                    </Grid>
+                    {count.map(item => (
+                        <Grid item xs={6}>
+                            <Paper className={classes.paper}>
+                                <TableContent />
+                            </Paper>
+                        </Grid>
+                    ))}
+                        
+                    
                 </Grid>
                 
             </div>
